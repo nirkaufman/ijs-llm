461b778bb2479cc589c04ac3c905ea0b7af6aa48
Nir Kaufman <nirkaufman@gmail.com>
2024-11-25 05:45:44 +0200

    init

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,4 +1,4 @@
-
+// OPENAI_API_KEY=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
 
 export default eventHandler((event) => {
   return "response";
